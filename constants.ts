90d0fd8fd015ef0d691e60bf62251470b8795808
Viki Val <viktorko99@gmail.com>
2023-07-31 18:54:54 +0200

    :zap: useChain

--- constants.ts
+++ constants.ts
@@ -7,3 +7,4 @@ export const KEYRING_SEED = Deno.env.get('KEYRING_SEED')!;
 export const METADATA = Deno.env.get('METADATA')!;
 export const COUNT = Deno.env.get('COUNT')!;
 export const START_INDEX = Deno.env.get('START_INDEX')!;
+export const CHAIN = Deno.env.get('CHAIN');
